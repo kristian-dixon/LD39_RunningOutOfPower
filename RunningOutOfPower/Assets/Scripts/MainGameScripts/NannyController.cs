b402d040cd1c59b7632d1fadecabcd6cf0ae42ea
KRISTIANDESKTOP\Kristian <kristiandixon1206@gmail.com>
2017-07-31 22:49:05 +0100

    House is practically done

--- RunningOutOfPower/Assets/Scripts/MainGameScripts/NannyController.cs
+++ RunningOutOfPower/Assets/Scripts/MainGameScripts/NannyController.cs
@@ -3,21 +3,103 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public class MapNode
+{
+    public float mTimeLastVisted;
+    public Vector3 mPostion;
+
+    public MapNode(Vector3 pPosition)
+    {
+        mPostion = pPosition;
+        mTimeLastVisted = 0;
+    }
+}
+
 public class NannyController : MonoBehaviour
 {
     NavMeshAgent mAgent;
 
-    public Transform mGoal;
+    public Transform mRoomContainer;
+
+    private List<MapNode> mNodes;
+    private int currentTarget;
+
+    float height;
 
 	// Use this for initialization
 	void Start ()
     {
         mAgent = GetComponent<NavMeshAgent>();
-        mAgent.destination = mGoal.position;
+
+        mNodes = new List<MapNode>();
+
+        if(mRoomContainer)
+        {
+            for(int i = 0; i < mRoomContainer.childCount; i++)
+            {
+                mNodes.Add(new MapNode(mRoomContainer.GetChild(i).position));
+            }
+        }
+
+        mAgent.SetDestination(ChooseNewTarget());
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	}
+        Debug.Log(mAgent.isStopped);
+
+        if (mAgent.remainingDistance == 0 )
+        {
+            mNodes[currentTarget].mTimeLastVisted = Time.time;
+            mAgent.SetDestination(ChooseNewTarget());
+        }
+
+    }
+
+    Vector3 ChooseNewTarget()
+    {
+        int bestChoice = -1;
+        float longestTimeWithoutVisit = -1;
+
+        //Go through list
+        //Find node that hasn't been visited for the longest
+
+        //TODO: Factor in distance
+        float currentTime = Time.time;
+
+        for(int i = 0; i < mNodes.Count; i++)
+        {
+            float timeSinceLastVisit = currentTime - mNodes[i].mTimeLastVisted;
+
+            if(timeSinceLastVisit > longestTimeWithoutVisit)
+            {
+                longestTimeWithoutVisit = timeSinceLastVisit;
+                bestChoice = i;
+            }
+            else if(timeSinceLastVisit == longestTimeWithoutVisit)
+            {
+                float thisDistance = Vector3.Distance(mNodes[i].mPostion, transform.position);
+                if(mNodes[i].mPostion.y != height)
+                {
+                    thisDistance *= 100f;
+                }
+                
+
+                float otherDistance = Vector3.Distance(mNodes[bestChoice].mPostion, transform.position);
+                if (mNodes[bestChoice].mPostion.y != height)
+                {
+                    otherDistance *= 100f;
+                }
+
+                if (thisDistance < otherDistance)
+                {
+                    bestChoice = i;
+                }
+            }
+        }
+        currentTarget = bestChoice;
+        height = mNodes[bestChoice].mPostion.y;
+        return mNodes[bestChoice].mPostion;
+    }
 }
